5e7bdc64006f27d25164e8cbb6564fd7671e30f2
vegirevathi <vegirevathi@gmail.com>
2020-05-20 17:19:32 +0530

    Entering Last name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -x
 echo "Welcome to User Registration"
-
+echo "Enter Last Name"
+read LastName
+LastNamePat="^[A-Z]{1}[a-z]*"
+if [[ $LastName =~ $LastNamePat ]]
+then
+	echo "valid"
+else
+	echo "invalid"
+fi
