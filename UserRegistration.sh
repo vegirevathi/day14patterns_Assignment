dd6e0b7053efbaa3bdc861e2d1c78f3dd7d04a38
vegirevathi <vegirevathi@gmail.com>
2020-05-20 18:19:22 +0530

    cehcking for rule 3

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -x
 echo "Welcome to User Registration"
-
+echo "Checking for special character"
+read password
+if [[ $password =~ [.,@#%^*+-] ]]
+then
+	echo "Rule 3 is passed"
+else
+	echo "Rule 3 is failed"
+fi
