9f48513fd8f52ca37ac7717c799212fce1b3a86a
vegirevathi <vegirevathi@gmail.com>
2020-05-20 17:35:06 +0530

    entering email id

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -x
 echo "Welcome to User Registration"
-
+echo "Enter a valid Email"
+read Email
+Emailpat=" ^abc[.][a-z]{3,}[@]["bl"][.]["co"][.][a-z]{2,}"
+if [[ $Email =~ $EmailPat ]]
+then
+	echo "valid"
+else
+	echo "Invalid"
+fi
