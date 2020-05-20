8b21f640221e415a173d6539cd51de032b6185dc
vegirevathi <vegirevathi@gmail.com>
2020-05-20 12:40:30 +0530

    email pattern part2 is modified

--- email.sh
+++ email.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
+shopt -s extglob
+
 echo "Enter Email Address"
 read email
+emailPat_part2='[.-+]?[100 111 1]?'
+echo $emailPat_part2
+if [[ $email =~ $emailPat_part2 ]];
+then
+	echo "valid";
+else
+	echo "invalid";
+fi
