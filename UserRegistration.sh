c26cdfd1611e8656f2768fc30c0ae4e2809f5180
vegirevathi <vegirevathi@gmail.com>
2020-05-20 18:14:27 +0530

    Checking password for rule 2

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
 echo "Welcome to User Registration"
 
+echo "enter password"
+read password
+if [[ $password =~ [0-9] ]]
+then
+	echo "Rule 2 is passed"
+else
+	echo "Rule 2 is failed"
+fi
+
