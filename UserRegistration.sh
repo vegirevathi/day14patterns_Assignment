2d9b6e0d5384236a80e0df421dcb1b581bfe3be5
vegirevathi <vegirevathi@gmail.com>
2020-05-20 17:58:17 +0530

    entering password

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -x
 echo "Welcome to User Registration"
-
+echo "enter password"
+read password
+passwordPat=[a-zA-Z/0-9]{8}
+if [[ $password =~ $passwordPat ]]
+then
+	echo "Password is valid"
+else
+	echo "Password is invalid"
+fi
