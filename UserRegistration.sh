620ce1d4d90a5d363080f0e5b2a622bedc876da6
vegirevathi <vegirevathi@gmail.com>
2020-05-20 17:43:07 +0530

    Entering Mobile Number

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -x
 echo "Welcome to User Registration"
-
+echo "Enter mobile number"
+read mobileNumber
+NumPat=^[91] [0-9]{10,}$
+if [[ $mobileNUmber =~ $NumPat ]];
+then
+	echo "mobile number is valid"
+else
+	echo "mobile number is invalid"
+fi
