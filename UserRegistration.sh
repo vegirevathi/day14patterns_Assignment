53b40e1036521e0a84929034c234da6693fdc68d
vegirevathi <vegirevathi@gmail.com>
2020-05-20 17:15:58 +0530

    Entering First Name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
+shopt -s extglob
 echo "Welcome to User Registration"
-
+echo "Enter the First Name"
+read FirstName
+FirstNamePat="^[A-Z]{1,}[a-z]*"
+if [[ $FirstName =~ $FirstNamePat ]];
+then
+	echo "Valid";
+else
+	echo "Invalid";
+fi
