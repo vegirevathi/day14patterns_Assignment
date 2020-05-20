b4910cff47e7c4826c65484196fc54c241f4fda2
vegirevathi <vegirevathi@gmail.com>
2020-05-20 18:24:40 +0530

    entering email id

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -2,7 +2,7 @@
 echo "Welcome to User Registration"
 echo "Enter a valid Email"
 read Email
-Emailpat=" ^(abc)+(.[a-z]{3,})+(@bl.co.)+([a-z]{2,})"
+Emailpat=" ^(abc)+[.]+[a-z]{3}@[bl.co.][a-z]{2,}"
 if [[ $Email =~ $EmailPat ]]
 then
 	echo "email is valid"
