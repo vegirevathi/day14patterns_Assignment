7cb0a4c8b21fbf11a304cde2117ff3928826bfa9
vegirevathi <vegirevathi@gmail.com>
2020-05-20 14:56:57 +0530

    email Pattern part2 is modified

--- email.sh
+++ email.sh
@@ -3,7 +3,7 @@ shopt -s extglob
 
 echo "Enter Email Address"
 read email
-emailPat_part2='[.-+]?[100 111 1]?'
+emailPat_part2='[.-+]?100|111'
 echo $emailPat_part2
 if [[ $email =~ $emailPat_part2 ]];
 then
