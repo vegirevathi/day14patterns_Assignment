64ef4b496d7289b0b968373f67283f340bc857be
vegirevathi <vegirevathi@gmail.com>
2020-05-20 18:11:05 +0530

    checking for rule1 in password

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -x
-echo "Welcome to User Registration"
 
+echo "Welcome to User Registration"
+echo "Checking for alphabet"
+read passwordPat
+if [[ $passwordPat =~ [a-z] ]]
+then
+	echo "Rule1 is passed"
+else
+	echo "rule1 is failed"
+fi
